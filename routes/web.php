28df7f731063a9dcbeb2dc2e059454ab76b14c52
Saad Javed <saad.developer218@gmail.com>
2025-05-18 16:29:32 +0500

    UI Sccaffoldings and Email Verify functioanlity added

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes(['verify' => true]);
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 });
